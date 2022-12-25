6d734e1deb469f4a980da99c2e42a6d7ec12049b
Shrish236 <85394223+Shrish236@users.noreply.github.com>
2022-12-25 11:03:55 +0530

    Update settings.py

--- sample_project/sample_project/settings.py
+++ sample_project/sample_project/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -126,7 +125,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
     ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-django_heroku.settings(locals())
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
